feb5508c74e6553f0a3fb84e72cd033e90ca6932
coding-wave-academy <kriskandi09@gmail.com>
2026-01-04 12:09:44 +0100

    testing-v1

--- ticha-frontend/vite.config.js
+++ ticha-frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://api-v1-ticha-backend.onrender.com/",
+        target: "https://localhost:5000",
         changeOrigin: true,
         secure: false,
       }
